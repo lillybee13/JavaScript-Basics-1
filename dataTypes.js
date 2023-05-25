05095bb1f51819ca14c523a6bc4a5dbd81c1434e
Melissa Cook <lillybee13@Melissas-Air.localdomain>
2023-05-25 17:19:25 -0600

    starting objects and classes

--- dataTypes.js
+++ dataTypes.js
@@ -28,13 +28,19 @@ if(age < 30){
     console.log("old fart")
 }
 
+//push adds to the end of an array
 fruits.push("orange")
+//pop removes the last item in the array
 fruits.pop()
 
+//unshift adds to the beginning of an array
 fruits.unshift("pear")
+//shift removes the first item in the array
 fruits.shift()
 
-let fruitsCopy = fruits.slice(1, 3)
+//slice makes a copy of the original array. Can take in 2 arguments; the starting index and the ending index. If no arguments are specified it copies the whole array. Ending index is not included inside the copy.
+let fruitsCopy = fruits.slice(1, 3) 
+//can take 3 arguments. 1st, starting index; 2nd, how many items; 3rd, what it is being replaced with. Splice alters the original array, slice makes a copy
 let middleFruits = fruits.splice(1, 2, "mango")
 
 console.log(fruits, middleFruits)
